7b710058860d01ab4c4a4bc4b7880db95732839c
nsa31 <n.sakib.adn@gmail.com>
2018-06-08 01:10:08 -0600

    Update fileTest.py

--- fileTest.py
+++ fileTest.py
@@ -9,3 +9,30 @@ file.write('Hello World')
 file.write('ok daone')
  
 file.close() 
+
+'''
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    pullData = open("sampleText.txt","r").read()
+    dataArray = pullData.split('\n')
+    xar = []
+    yar = []
+    for eachLine in dataArray:
+        if len(eachLine)>1:
+            x,y = eachLine.split(',')
+            xar.append(int(x))
+            yar.append(int(y))
+    ax1.clear()
+    ax1.plot(xar,yar)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+https://stackoverflow.com/questions/11874767/real-time-plotting-in-while-loop-with-matplotlib
+https://pythonspot.com/matplotlib-update-plot/
+http://pythonopencv.com/opencv-real-time-graph-plot-using-matplotlib/
+'''
